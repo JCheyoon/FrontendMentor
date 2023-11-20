f368e20d3e6e908bceb0f927221529da9d8727be
Cheyoon Jung <jessietwo94@gmail.com>
2023-11-20 10:54:29 +0100

    update step

--- multi-step-form/src/Components/Step.component.tsx
+++ multi-step-form/src/Components/Step.component.tsx
@@ -1,8 +1,36 @@
 import "./Step.style.scss";
+import { useLocation } from "react-router";
 const Step = () => {
+  const currentRoute = useLocation().pathname;
+  console.log(currentRoute);
+
+  const stepList = [
+    { number: 1, title: "YOUR INFO", route: "/" },
+    { number: 2, title: "SELECT PLAN", route: "/plan" },
+    { number: 3, title: "ADD-ONS", route: "/addon" },
+    { number: 4, title: "SUMMARY", route: "/summary" },
+  ];
   return (
     <div className="step-container">
-      <li>dd</li>
+      <ul>
+        {stepList.map((list) => (
+          <li key={list.number}>
+            <div className="step-box">
+              <div
+                className={`number ${
+                  currentRoute === list.route ? "active" : ""
+                }`}
+              >
+                {list.number}
+              </div>
+              <div className="step-title">
+                <span>STEP&nbsp;{list.number}</span>
+                <p>{list.title}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
